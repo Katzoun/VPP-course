a0a17fa960af863ce08fec88ace7883f95587e54
Katzoun <tomas.janousek02@gmail.com>
2025-09-26 12:29:44 +0200

    ex3 maybe finished

--- ex/ex3/numerical_ex3.m
+++ ex/ex3/numerical_ex3.m
@@ -1,43 +1,42 @@
 clear;clc;
 
-b = 1;
-N = 10; 
-
-x0 = 1
-Xs = [1,2,3,4,5];
-
-gp= [1, 0.9, 0.6, 0.4, 0]; %gp(x_k)   
-ga = [0, -0.4, -1]; %ga(x_k)  
-
-g_k @(x,u)
-g_N = @(x) 0;
-
-f_k = @(x,u,w) x-u;
-
-
 N = 10; S = 5; A = 3;              % horizon, states, actions
-
-
 
 % P(:,:,u+1) -- transition matrix for action u
 P = zeros(S,S,A);
 
 % u = 0 (do nothing)
-P(:,:,1) = [0.6 0.3 0.1 0   0 ;
-            0   0.7 0.2 0.1 0 ;
-            0   0   0.75 0.15 0.10;
-            0   0   0    0.8 0.2;
-            0   0   0    0   1.0];
+P(:,:,1) = [0.60, 0.30, 0.10, 0.00, 0.00;
+                 0.00, 0.70, 0.20, 0.10, 0.00;
+                 0.00, 0.00, 0.75, 0.15, 0.10;
+                 0.00, 0.00, 0.00, 0.80, 0.20;
+                 0.00, 0.00, 0.00, 0.00, 1.00];
 
 % u = 1 (repair)
-P(:,:,2) = [1   0   0   0   0;
-            0.9 0.1 0   0   0;
-            0   0.9 0.1 0   0;
-            0   0   0.9 0.1 0;
-            0   0   0   0.9 0.1];
+P(:,:,2) = [1.00, 0.00, 0.00, 0.00, 0.00; 
+                 0.90, 0.10, 0.00, 0.00, 0.00;
+                 0.00, 0.90, 0.10, 0.00, 0.00;
+                 0.00, 0.00, 0.90, 0.10, 0.00;
+                 0.00, 0.00, 0.00, 0.90, 0.10];
 
 % u = 2 (buy new)
-P(:,:,3) = repmat([1 0 0 0 0], S, 1);
+P(:,:,3) =[1 0 0 0 0;
+                1 0 0 0 0;
+                1 0 0 0 0;
+                1 0 0 0 0;
+                1 0 0 0 0;];
+
+Xs = [1,2,3,4,5];
+Us = [0,1,2];
+
+gp= [1, 0.9, 0.6, 0.4, 0]; %gp(x_k)   
+ga = [0, -0.4, -1]; %ga(u_k)  
+
+g_N = @(x) - gp(x); %zaporne kvuli minimalizaci
+g_k = @(x,u)  -( gp(x) + ga(u+1));
+
+f_k = @(x,u,w) w; %x_k+1
+
 
 J = zeros(length(Xs),N+1);
 mu = zeros(length(Xs),N);
@@ -47,17 +46,16 @@ for i=1:length(Xs)
     J(i,N+1) = g_N(x_cur);
 end
 
+
 for k=N:-1:1
     for i=1:length(Xs)
         x_cur = Xs(i);
-        Us = Xs(Xs <= x_cur);
         J_sub = zeros(length(Us),1);
         for ii=1:length(Us)
             u_cur = Us(ii);
-            x_next = f_k(x_cur,u_cur,0);
+            Ws = P(x_cur,:,u_cur+1); %u_cur+1 kvuli indexum
+            J_sub(ii) = g_k(x_cur,u_cur) + dot(Ws,J(:,k+1));
 
-            id_x_next = find(abs(Xs - x_next) < tol);
-            J_sub(ii) = (g_k(x_cur,u_cur,0) + alpha*J(id_x_next,k+1));
         end
         [minval,minpos] = min(J_sub);
         J(i,k) = minval; mu(i,k) = Us(minpos);
@@ -65,24 +63,27 @@ for k=N:-1:1
 end
 
 %% simulation
+n_sim = 10000; x0 = 1;
+J_sim = zeros(n_sim,1); x_sim = zeros(n_sim,N+1);
+x_sim(:,1) = x0;
+for s=1:n_sim
+    for k=1:N
+        x_cur_sim = x_sim(s,k);
+        u_cur_sim = mu(x_cur_sim,k);
+        Ws_sim = P(x_cur_sim,:,u_cur_sim+1); 
+        % c =  cumsum(Ws_sim);
+        % r = rand < c;
+        % ids = find(r);
+        %zjednodusene
+        ids = find(rand < cumsum(Ws_sim));
+        w_sim = Ws_sim(ids(1));
 
-x0 = b;
-J(find(abs(Xs - x0) < tol),1) %optimalni cost-to-go z x0
-u0 = mu(find(abs(Xs - x0) < tol),1);
-x1 = x0 - u0;
+        J_sim(s) = J_sim(s) + g_k(x_cur_sim,u_cur_sim);
+        x_sim(s,k+1) = ids(1); 
+    end
+    J_sim(s) = J_sim(s) + g_N(x_sim(s, N+1));
+end
 
-u1 = (mu(find(abs(Xs - x1) < tol),2));
-x2 = x1 - u1;
-
-u2 = (mu(find(abs(Xs - x2) < tol),3));
-x3 = x2 - u2;
-
-% u2 = x2
-[u0,u1,u2]
-
-
-
-
-
-
+fprintf('Prumerna hodnota zisku ze simulace: %.4d \n' , mean(J_sim)); %zaporne kvuli minimalizaci
+fprintf('Optimalni hodnota zisku z DP: %.4d \n' ,  J(1,1)); %zaporne kvuli minimalizaci
 
